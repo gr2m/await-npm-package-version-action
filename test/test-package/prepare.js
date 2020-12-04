dc105fc5d3251a6fcccab0b3393c6ed5cb681d61
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-12-03 23:04:37 -0800

    debug

--- test/test-package/prepare.js
+++ test/test-package/prepare.js
@@ -1,16 +1,8 @@
 const { writeFileSync } = require("fs");
 const core = require("@actions/core");
 
-if (!process.env.NPM_TOKEN) {
-  throw new Error("NPM_TOKEN is not set");
-}
-
 const pkg = require("./package.json");
 pkg.version = "1.0.0-test-" + Date.now();
 
 writeFileSync("package.json", JSON.stringify(pkg, null, 2) + "\n");
-writeFileSync(
-  ".npmrc",
-  `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}`
-);
 core.setOutput("version", pkg.version);
